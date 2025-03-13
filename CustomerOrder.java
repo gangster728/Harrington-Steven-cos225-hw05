35f6d134493b3fb8a308dd737deb71ed77ce1d3e
gangster728 <stevenharrington728@gmail.com>
2025-03-13 12:02:27 -0400

    classes initialized
    made classes to hold the code

---
+++ CustomerOrder.java
@@ -0,0 +1,18 @@
+public class CustomerOrder {
+    public String name;
+    public String date;
+    public int quantity;
+
+    public CustomerOrder(){
+    }
+    public CustomerOrder(int quantity){
+        this.quantity = quantity;
+    }
+
+    public void ShipProduct(int quantity){
+        if (quantity > 0){
+          quantity--;  
+        }
+        
+    }
+}
